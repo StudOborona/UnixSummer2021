f3ba98f6b0923769ed602d3a65d9a2f1c6c58c6a
Demka <GeorgiyDemo@users.noreply.github.com>
2021-06-08 20:16:15 +0300

    Краткое решение 5

--- task5/task5_3.py
+++ task5/task5_3.py
@@ -3,11 +3,23 @@
 Количество потоков программы должно определяться параметром командной строки.
 --
 Существует несколько вариантов решения, это один из них
+Краткая форма
 """
+import threading
+
+pi = 1
+
+
+def counter(n):
+    global pi
+    pi += (1 / (n ** 2))
 
 
 def main():
-    pass
+    a = int(input("Введите кол-во потоков -> "))
+    for i in range(a):
+        threading.Thread(target=counter, args=(i + 2,)).start()
+    print(pow((pi * 6), 0.5))
 
 
 if __name__ == "__main__":
