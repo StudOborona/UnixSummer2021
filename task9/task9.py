489530d373b8c8bbcaf0f7c996ec2149864efc22
Demka <GeorgiyDemo@users.noreply.github.com>
2021-06-08 14:02:09 +0300

    Задание 10 есть #1

--- task9/task9.py
+++ task9/task9.py
@@ -1,6 +1,10 @@
 """
 Напишите программу, которая выводит на экран список номеров открытых портов на данной машине.
 Использовать команду netstat.
+
+--
+Можно использовать команду netstat с флагами -atun или -l на ваш выбор
+Python в данном случае выступает просто в виде обёртки
 """
 import subprocess
 
@@ -8,7 +12,6 @@ import subprocess
 def main():
     # Дергаем команду netstat через subprocess
     result = subprocess.getoutput("netstat -atun")
-    # или можно через netstat -l
     print(result)
 
 
