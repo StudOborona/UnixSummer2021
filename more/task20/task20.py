14476b22dce8e72ef11b2fcaf1d3e730369cdb02
Demka <GeorgiyDemo@users.noreply.github.com>
2021-06-20 21:24:55 +0300

    20 задание есть

--- more/task20/task20.py
+++ more/task20/task20.py
@@ -2,10 +2,26 @@
 Напишите программу, которая выводит список всех выполняющихся в текущий момент системных демонов.
 Рядом с каждым вывести адрес его конфигурационного файла.
 """
+import os
+import subprocess
 
 
 def main():
-    print("task20")
+    # Команда для получения всех демонов, которые работают сейчас
+    my_command = "systemctl --type=service --state=active | awk '{print $1}'"
+
+    # Создаем процесс с выполнением этой команды
+    p = subprocess.Popen(my_command, stdout=subprocess.PIPE, shell=True)
+
+    # Получаем результат работы процесса
+    out, err = p.communicate()
+
+    # Формируем список демонов, которые присутствуют в системе
+    daemon_list = [d for d in out.decode().split("\n") if ".service" in d]
+
+    # Для каждого демона выполняем команду systemctl show -p FragmentPath, которая выводит путь до его файла
+    for d in daemon_list:
+        os.system(f"echo '\n{d}' && systemctl show -p FragmentPath {d} | sed 's/FragmentPath=//g'")
 
 
 if __name__ == "__main__":
